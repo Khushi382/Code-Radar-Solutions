941b525d543bffbe61971ac341cf679593e48fc3
Khushi382 <ky20057777@gmail.com>
2025-02-03 10:20:38 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -17,9 +17,9 @@ int main() {
         case '/':
         printf("%d",a/b);
         break;
-        default:
-        printf("error");
-        break;    
+        //default:
+        //printf("error");
+        //break;    
 }
     return 0;
 }
